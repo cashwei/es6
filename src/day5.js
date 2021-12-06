db4609d19a7e80924dc02f4590a3ddcdd4fa50ed
cashwei <295108054@qq.com>
2021-12-06 14:33:50 +0800

    feat(day5.js): 添加了一小段代码啦啦
    哈哈哈哈哈哈
    
    BREAKING CHANGE: 呵呵呵

--- src/day5.js
+++ src/day5.js
@@ -104,3 +104,16 @@ genObj.next('a')
 // 1. a
 genObj.next('b')
 // 2. b
+function* foo() {
+  yield 1;
+  yield 2;
+  yield 3;
+  yield 4;
+  yield 5;
+  return 6;
+}
+
+for (let v of foo()) {
+  console.log(v);
+}
+// 1 2 3 4 5
